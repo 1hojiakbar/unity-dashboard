4fd25ea9f588c6708af55b60d783025c981d80bd
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-07-29 21:33:49 +0500

    some changed

--- src/pages/Overview/RightSection.tsx
+++ src/pages/Overview/RightSection.tsx
@@ -1,7 +1,7 @@
+import { Bar } from "react-chartjs-2";
 import { Input } from "../../components/Generic";
 import bellIcon from "../../assets/icons/bell.svg";
 import searchIcon from "../../assets/icons/search.svg";
-import { Bar } from "react-chartjs-2";
 import { barChartData, barChartOptions } from "../../utils/overview";
 
 const RightSection = () => {
@@ -14,26 +14,33 @@ const RightSection = () => {
             className="outline-none p-1 placeholder:font-semibold"
             placeholder="Search"
             iconLeft={searchIcon}
+            aria-label="Search"
           />
         </div>
         <div className="flex justify-end w-1/4">
-          <div className="p-2 relative cursor-pointer">
+          <div
+            className="p-2 relative cursor-pointer"
+            aria-label="Notifications"
+          >
             <div className="flex w-5 h-5 text-sm items-center justify-center text-white absolute top-0 right-0 bg-notfication-bg rounded-full">
               2
             </div>
-            <img className="w-7" src={bellIcon} alt="icon not found" />
+            <img className="w-7" src={bellIcon} alt="Notification icon" />
           </div>
         </div>
       </nav>
       <div className="flex items-center justify-center mt-10 h-max bg-white">
         <div className="bg-white rounded-xl shadow-2xl p-6 w-80 text-center py-12">
-          <h2 className="text-gray-500 mb-2">Your earning this month</h2>
+          <h2 className="text-gray-500 mb-2">Your earnings this month</h2>
           <p className="text-5xl font-bold text-purple-600 mb-4">479.4</p>
           <p className="text-gray-400 mb-6">
             Update your payout method in Settings
           </p>
-          <button className="bg-white text-active-color py-2 px-4 rounded-xl transition-all ease-linear hover:bg-active-color hover:text-white outline-none">
-            Withdraw All Earning
+          <button
+            className="bg-white text-active-color py-2 px-4 rounded-xl transition-all ease-linear hover:bg-active-color hover:text-white outline-none"
+            aria-label="Withdraw All Earnings"
+          >
+            Withdraw All Earnings
           </button>
         </div>
       </div>
